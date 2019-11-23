dd4113310d93bd64a84327e629d97a52d9c1de7c
Surya Prasad <techniboy.h2o@gmail.com>
2019-11-23 02:23:58 +0000

    add function to interpret state message

---
+++ protocol/move_turn.go
@@ -0,0 +1,37 @@
+package protocol
+
+type MoveTurn struct {
+	end   bool
+	again bool
+	move  int
+}
+
+// getter for end in MoveTurn
+func (moveTurn MoveTurn) End() bool {
+	return moveTurn.end
+}
+
+// getter for again in MoveTurn
+func (moveTurn MoveTurn) Again() bool {
+	return moveTurn.again
+}
+
+// getter for move in MoveTurn
+func (moveTurn MoveTurn) Move() int {
+	return moveTurn.move
+}
+
+// setter for end in MoveTurn
+func (moveTurn *MoveTurn) SetEnd(end bool) {
+	moveTurn.end = end
+}
+
+// setter for again in MoveTurn
+func (moveTurn *MoveTurn) SetAgain(again bool) {
+	moveTurn.again = again
+}
+
+// setter for move in MoveTurn
+func (moveTurn *MoveTurn) SetMove(move int) {
+	moveTurn.move = move
+}
--- protocol/protocol.go
+++ protocol/protocol.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -38,11 +39,48 @@ func GetMsgType(msg string) string {
 	} else if strings.HasPrefix(msg, "END\n") {
 		return END
 	} else {
-		return "invalidMessageError: could not determine message type"
+		return "invalidMessageError: could not determine message type."
 	}
 
 }
 
+// Interprets a "state_change" message. Should be called if
+// GetMsgType(msg) returns MsgType.STATE
 func InterpretStateMsg(msg string) MoveTurn {
+	moveTurn := MoveTurn{}
 
+	if msg[len(msg)-1] != '\n' {
+		fmt.Println("invalidMessageError: message not terminated with 0x0A character.")
+	}
+
+	msgParts := strings.SplitN(msg, ":", 4)
+	if len(msgParts) != 4 {
+		fmt.Println("invalidMessageError: missing arguments.")
+	}
+
+	// msgParts[0] is "CHANGE"
+	// 1st argument: the move (or swap)
+	if msgParts[1] == "SWAP" {
+		moveTurn.SetMove(0)
+	} else {
+		move, err := strconv.Atoi(msgParts[1])
+		if err != nil {
+			panic(err)
+		}
+		moveTurn.SetMove(move)
+	}
+
+	// 3rd argument: whose turn
+	moveTurn.SetEnd(false)
+	if msgParts[3] == "YOU\n" {
+		moveTurn.SetAgain(true)
+	} else if msgParts[3] == "OPP\n" {
+		moveTurn.SetAgain(false)
+	} else if msgParts[3] == "END\n" {
+		moveTurn.SetEnd(true)
+		moveTurn.SetAgain(false)
+	} else {
+		fmt.Printf("invalidMessageError: illegal value for turn parameter")
+	}
+	return moveTurn
 }
