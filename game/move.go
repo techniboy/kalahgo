e0c4d939776aedb586eaa54d7485348361153fa4
Surya Prasad <techniboy.h2o@gmail.com>
2019-11-25 00:52:36 +0000

    change Side type to pointer in move

--- game/move.go
+++ game/move.go
@@ -4,12 +4,13 @@ import "errors"
 
 // Move represents a whole (if greater than 1) or the pie action if 0.
 type Move struct {
-	Side  Side
+	Side  *Side
 	Index int
 }
 
 // NewMove is a constructor for Move, might be needed or not LOL
-func (m *Move) NewMove(side Side, index int) (*Move, error) {
+func NewMove(side *Side, index int) (*Move, error) {
+	m := new(Move)
 	if index < 0 || index > 7 {
 		return nil, errors.New("valueError: move number must be strictly greater than 0 and less than 8")
 	}
