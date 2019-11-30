cac1f661401df1ee781dc34065e3cf400413a531
Surya Prasad <techniboy.h2o@gmail.com>
2019-11-30 21:35:06 +0000

    add policies and mcts driver

--- agent/mcts/mcts.go
+++ agent/mcts/mcts.go
@@ -1,1 +1,31 @@
 package mcts
+
+import (
+	"log"
+	"time"
+
+	"github.com/techniboy/kalahgo/agent/mcts/graph"
+	"github.com/techniboy/kalahgo/agent/mcts/policy"
+	"github.com/techniboy/kalahgo/game"
+)
+
+type MCTS struct {
+	RunTime time.Duration
+}
+
+func (mcts *MCTS) Search(state *game.MancalaEnv) *game.Move {
+	gameStateRoot := graph.NewNode(state.Clone(), nil, nil)
+	gamesPlayed := 0
+	startTime := time.Now()
+	for time.Now().Sub(startTime) < mcts.RunTime {
+		node := policy.MctpSelect(gameStateRoot)
+		policy.McdpSimuilate(node)
+		graph.Backpropagate(node)
+		gamesPlayed++
+	}
+	maxChild, err := graph.SelectMaxChild(gameStateRoot)
+	if err != nil {
+		log.Panic(err)
+	}
+	return maxChild.Move
+}
--- agent/mcts/policy/default.go
+++ agent/mcts/policy/default.go
@@ -1,1 +1,14 @@
 package policy
+
+import (
+	"math/rand"
+
+	"github.com/techniboy/kalahgo/agent/mcts/graph"
+)
+
+func McdpSimuilate(n *graph.Node) {
+	for !n.IsTerminal() {
+		randomLegalMove := n.State.LegalMoves()[rand.Intn(len(n.State.LegalMoves()))]
+		n.Update(float64(n.State.PerformMove(randomLegalMove)))
+	}
+}
